d3fcb75b154607ac2b7a3d89c329360650a7a023
Anshul Choudhary <95236614+AnshulChoudhary001@users.noreply.github.com>
2023-10-05 10:17:19 +0530

    Update Getting Started with C++.cpp

--- Getting Started with C++.cpp
+++ Getting Started with C++.cpp
@@ -1,1 +1,244 @@
-Code samples & snippets coming soon!
+Certainly! Below are some basic C++ code samples and snippets to help you get started with the language:
+
+### 1. **Hello, World!**
+
+```cpp
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+```
+
+This program prints "Hello, World!" to the console.
+
+### 2. **Variables and Data Types**
+
+```cpp
+#include <iostream>
+#include <string>
+
+int main() {
+    int age = 25;
+    float pi = 3.14;
+    double gravity = 9.81;
+    char grade = 'A';
+    std::string name = "John";
+
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Grade: " << grade << std::endl;
+    std::cout << "Pi: " << pi << std::endl;
+    std::cout << "Gravity: " << gravity << std::endl;
+
+    return 0;
+}
+```
+
+This program demonstrates different data types and how to declare variables in C++.
+
+### 3. **User Input**
+
+```cpp
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
+
+    return 0;
+}
+```
+
+This program takes user input (name) and prints a personalized greeting message.
+
+### 4. **Arithmetic Operations**
+
+```cpp
+#include <iostream>
+
+int main() {
+    int num1 = 10, num2 = 5;
+    int sum = num1 + num2;
+    int difference = num1 - num2;
+    int product = num1 * num2;
+    float quotient = (float)num1 / num2; // Type casting to float for accurate division
+
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    std::cout << "Quotient: " << quotient << std::endl;
+
+    return 0;
+}
+```
+
+This program performs basic arithmetic operations (addition, subtraction, multiplication, division) on two numbers.
+
+### 5. **Conditional Statements (if-else)**
+
+```cpp
+#include <iostream>
+
+int main() {
+    int num = 10;
+
+    if (num > 0) {
+        std::cout << "Number is positive." << std::endl;
+    } else if (num < 0) {
+        std::cout << "Number is negative." << std::endl;
+    } else {
+        std::cout << "Number is zero." << std::endl;
+    }
+
+    return 0;
+}
+```
+
+This program checks whether a number is positive, negative, or zero using conditional statements.
+
+
+### 6. **Loops (for, while, do-while)**
+
+#### For Loop:
+
+```cpp
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << "Iteration " << i << std::endl;
+    }
+    return 0;
+}
+```
+
+This program demonstrates a `for` loop that iterates from 1 to 5 and prints the iteration number.
+
+#### While Loop:
+
+```cpp
+#include <iostream>
+
+int main() {
+    int i = 1;
+    while (i <= 5) {
+        std::cout << "Iteration " << i << std::endl;
+        ++i;
+    }
+    return 0;
+}
+```
+
+Here, a `while` loop achieves the same result as the `for` loop.
+
+#### Do-While Loop:
+
+```cpp
+#include <iostream>
+
+int main() {
+    int i = 1;
+    do {
+        std::cout << "Iteration " << i << std::endl;
+        ++i;
+    } while (i <= 5);
+    return 0;
+}
+```
+
+The `do-while` loop ensures that the loop body executes at least once before checking the condition.
+
+### 7. **Arrays**
+
+```cpp
+#include <iostream>
+
+int main() {
+    int numbers[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Element at index " << i << ": " << numbers[i] << std::endl;
+    }
+
+    return 0;
+}
+```
+
+This code snippet creates an array of integers and prints the elements using a `for` loop.
+
+### 8. **Functions**
+
+```cpp
+#include <iostream>
+
+// Function declaration
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int result = add(3, 5);
+    std::cout << "Sum: " << result << std::endl;
+    return 0;
+}
+```
+
+In this example, a function named `add` takes two integers as parameters and returns their sum.
+
+### 9. **Classes and Objects**
+
+```cpp
+#include <iostream>
+#include <string>
+
+// Class declaration
+class Car {
+public:
+    std::string brand;
+    std::string model;
+
+    void displayInfo() {
+        std::cout << "Brand: " << brand << ", Model: " << model << std::endl;
+    }
+};
+
+int main() {
+    // Creating an object of the Car class
+    Car myCar;
+    myCar.brand = "Toyota";
+    myCar.model = "Camry";
+
+    // Calling a class method
+    myCar.displayInfo();
+    return 0;
+}
+```
+
+Here, `Car` is a class with member variables `brand` and `model`, and a member function `displayInfo` to print car information.
+
+### 10. **Pointers**
+
+```cpp
+#include <iostream>
+
+int main() {
+    int num = 10;
+    int* ptr = &num; // Pointer variable storing the memory address of num
+
+    std::cout << "Value of num: " << num << std::endl;
+    std::cout << "Memory address of num: " << &num << std::endl;
+    std::cout << "Value stored in pointer (memory address of num): " << ptr << std::endl;
+    std::cout << "Value at the memory address (dereferencing pointer): " << *ptr << std::endl;
+
+    return 0;
+}
+```
+
+In this example, a pointer variable (`ptr`) stores the memory address of an integer variable (`num`). The pointer is then dereferenced to access the value at that memory address.
+
+Feel free to experiment with these examples and modify them to understand different C++ concepts better. If you have specific questions or topics you'd like to learn more about, feel free to ask!
